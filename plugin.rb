10606cd624c5c99930c0c780b3b8943f6017740e
Francisco Jiménez Cabrera <jkfran@gmail.com>
2023-01-06 10:08:31 +0000

    Revert "Add Plugin settings"

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,7 @@
 # name: markdown-note
 # about: A BBCode Markdown extension for notes
-# version: 0.0.3
+# version: 0.0.2
 # authors: Robin Winslow
-# url: https://github.com/canonical/discourse-markdown-note
 
-enabled_site_setting :markdown_note_enabled
 register_asset "stylesheets/notifications.scss"
+
